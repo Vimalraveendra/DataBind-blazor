bfbd31812960435abd27f07fadf3e196203339a0
Vimal Raveendran <vimalraveendra@gmail.com>
2021-07-11 20:45:33 +0200

    added SelectComponent, TwoWayDataBind, RadioComponent & CheckBox  components

--- DataBind/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/CheckBox.razor.g.cs
+++ DataBind/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/CheckBox.razor.g.cs
@@ -75,8 +75,7 @@ using DataBind.Shared;
 #line default
 #line hidden
 #nullable disable
-    [Microsoft.AspNetCore.Components.RouteAttribute("/")]
-    public partial class Index : Microsoft.AspNetCore.Components.ComponentBase
+    public partial class CheckBox : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
@@ -84,14 +83,13 @@ using DataBind.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 17 "/Users/vimalraveendran/Desktop/blazor/DataBind/DataBind/Pages/Index.razor"
+#line 8 "/Users/vimalraveendran/Desktop/blazor/DataBind/DataBind/Pages/CheckBox.razor"
        
-    public string MyName { get; set; }
+    public bool AgreeValue { get; set; }
 
-    private void NameChange(ChangeEventArgs e)
+    private void Agree(ChangeEventArgs e)
     {
-        MyName = e.Value as string;
-       
+        AgreeValue = Convert.ToBoolean(e.Value);
     }
 
 #line default
--- DataBind/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/SelectComponent.razor.g.cs
+++ DataBind/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/SelectComponent.razor.g.cs
@@ -75,8 +75,7 @@ using DataBind.Shared;
 #line default
 #line hidden
 #nullable disable
-    [Microsoft.AspNetCore.Components.RouteAttribute("/")]
-    public partial class Index : Microsoft.AspNetCore.Components.ComponentBase
+    public partial class SelectComponent : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
@@ -84,14 +83,13 @@ using DataBind.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 17 "/Users/vimalraveendran/Desktop/blazor/DataBind/DataBind/Pages/Index.razor"
+#line 12 "/Users/vimalraveendran/Desktop/blazor/DataBind/DataBind/Pages/SelectComponent.razor"
        
-    public string MyName { get; set; }
-
-    private void NameChange(ChangeEventArgs e)
+    public string MyOption { get; set; }
+        
+    private void OptionHandle(ChangeEventArgs e)
     {
-        MyName = e.Value as string;
-       
+        MyOption = e.Value as string;
     }
 
 #line default
